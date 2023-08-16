e5a50f349f199e746af4f17c9672d38fef4ba093
osamb <omarsamb955@gmail.com>
2023-08-16 12:12:23 +0000

    update

--- database/structure.sql
+++ database/structure.sql
@@ -1,5 +1,3 @@
--- schema.sql
-
 -- Create the users table
 CREATE TABLE IF NOT EXISTS users (
     id INTEGER PRIMARY KEY,
@@ -9,15 +7,22 @@ CREATE TABLE IF NOT EXISTS users (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
+-- Create the categories table
+CREATE TABLE IF NOT EXISTS categories (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL
+);
+
 -- Create the posts table
 CREATE TABLE IF NOT EXISTS posts (
     id INTEGER PRIMARY KEY,
     user_id INTEGER NOT NULL,
     title TEXT NOT NULL,
     content TEXT NOT NULL,
-    category TEXT,
+    category_id INTEGER NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES users(id)
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (category_id) REFERENCES categories(id)
 );
 
 -- Create the comments table
@@ -31,8 +36,8 @@ CREATE TABLE IF NOT EXISTS comments (
     FOREIGN KEY (post_id) REFERENCES posts(id)
 );
 
--- Create the likes table
-CREATE TABLE IF NOT EXISTS likes (
+-- Create the likes table for posts
+CREATE TABLE IF NOT EXISTS post_likes (
     id INTEGER PRIMARY KEY,
     user_id INTEGER NOT NULL,
     post_id INTEGER NOT NULL,
@@ -41,12 +46,32 @@ CREATE TABLE IF NOT EXISTS likes (
     FOREIGN KEY (post_id) REFERENCES posts(id)
 );
 
--- Create the dislikes table
-CREATE TABLE IF NOT EXISTS dislikes (
+-- Create the dislikes table for posts
+CREATE TABLE IF NOT EXISTS post_dislikes (
     id INTEGER PRIMARY KEY,
     user_id INTEGER NOT NULL,
     post_id INTEGER NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES users(id),
     FOREIGN KEY (post_id) REFERENCES posts(id)
+);
+
+-- Create the likes table for comments
+CREATE TABLE IF NOT EXISTS comment_likes (
+    id INTEGER PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    comment_id INTEGER NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (comment_id) REFERENCES comments(id)
+);
+
+-- Create the dislikes table for comments
+CREATE TABLE IF NOT EXISTS comment_dislikes (
+    id INTEGER PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    comment_id INTEGER NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (comment_id) REFERENCES comments(id)
 );
