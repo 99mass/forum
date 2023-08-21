0b237c5bb12748f176de1a6bc7451acc62870e3e
ssambadi <sambadiop161@gmail.com>
2023-08-21 17:37:01 +0000

    some features

--- static/js/script.js
+++ static/js/script.js
@@ -1,10 +1,9 @@
 const faBars=document.querySelector('.fa-bars');
 const contentLink=document.querySelector('.content-link');
-// console.log(contentLink);
 
-window.onload = () => {
+
     contentLink.style.display="none";
-    faBars.addEventListener("click", (event) => {
+    faBars.addEventListener("click", () => {
         
         if (contentLink.style.display === "none") {
             contentLink.style.display = "block";
@@ -12,4 +11,3 @@ window.onload = () => {
             contentLink.style.display = "none";
         }
     });
-};
