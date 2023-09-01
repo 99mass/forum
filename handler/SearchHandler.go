dc6fb5e20588e8c493976ec0511cf4b614d7501a
mouhametadiouf <diegargonefaye@gmail.com>
2023-09-01 13:45:01 +0000

    add checkrequest for search handler

--- handler/SearchHandler.go
+++ handler/SearchHandler.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"forum/controller"
 	"forum/helper"
+	"forum/middlewares"
 	"forum/models"
 	"net/http"
 	"strings"
@@ -12,38 +13,42 @@ import (
 
 func Search(db *sql.DB) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		switch r.Method {
-		case http.MethodPost:
-			title := r.FormValue("title")
-			title = strings.TrimSpace(title)
-			title = strings.ToLower(title)
-			Datas, err := helper.GetDataTemplate(db, r, true, false, false, false, false)
+		ok, errorPage := middlewares.CheckRequest(r, "/search", "post")
+		if !ok {
+			helper.ErrorPage(w, errorPage)
+			return
+		}
+
+		title := r.FormValue("title")
+		title = strings.TrimSpace(title)
+		title = strings.ToLower(title)
+		Datas, err := helper.GetDataTemplate(db, r, true, false, false, false, false)
+		if err != nil {
+			helper.ErrorPage(w, http.StatusBadRequest)
+			return
+		}
+		posts, err := controller.GetPostsByTitle(db, title)
+		if err != nil {
+			Datas.Error = "there's no post for this title"
+		}
+		var postsDetails []models.HomeDataPost
+		for _, v := range posts {
+			v.Categories, err = controller.GetCategoriesByPost(db, v.ID)
 			if err != nil {
 				helper.ErrorPage(w, http.StatusBadRequest)
 				return
 			}
-			posts, err := controller.GetPostsByTitle(db, title)
+			fmt.Println(v.CreatedAt)
+			post, err := helper.GetDetailPost(db, v)
 			if err != nil {
-				Datas.Error = "there's no post for this title"
+				helper.ErrorPage(w, http.StatusBadRequest)
+				return
 			}
-			var postsDetails []models.HomeDataPost
-			for _, v := range posts {
-				v.Categories, err = controller.GetCategoriesByPost(db, v.ID)
-				if err != nil {
-					helper.ErrorPage(w, http.StatusBadRequest)
-					return
-				}
-				fmt.Println(v.CreatedAt)
-				post, err := helper.GetDetailPost(db, v)
-				if err != nil {
-					helper.ErrorPage(w, http.StatusBadRequest)
-					return
-				}
 
-				postsDetails = append(postsDetails, post)
-			}
-			Datas.Datas = postsDetails
-			helper.RenderTemplate(w, "index", "index", Datas)
+			postsDetails = append(postsDetails, post)
 		}
+		Datas.Datas = postsDetails
+		helper.RenderTemplate(w, "index", "index", Datas)
 	}
+
 }
