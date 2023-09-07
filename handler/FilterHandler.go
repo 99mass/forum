3a50313136e560fa839e022aa998edd15a026f50
osamb <omarsamb955@gmail.com>
2023-09-07 17:21:59 +0000

    update

--- handler/FilterHandler.go
+++ handler/FilterHandler.go
@@ -11,12 +11,18 @@ import (
 
 	"forum/controller"
 	"forum/helper"
+	"forum/middlewares"
 	"forum/models"
 )
 
 func Filter(db *sql.DB) http.HandlerFunc {
 
 	return func(w http.ResponseWriter, r *http.Request) {
+		ok, errorPage := middlewares.CheckRequest(r, "/filter", "post")
+		if !ok {
+			helper.ErrorPage(w, errorPage)
+			return
+		}
 		err := r.ParseForm()
 		if err != nil {
 			helper.ErrorPage(w, http.StatusBadRequest)
