647b6ba2c0fa55006737b553a5a07c60e91ab7b6
osamb <omarsamb955@gmail.com>
2023-09-11 12:49:15 +0000

    update filter for my page

--- routes/route.go
+++ routes/route.go
@@ -24,4 +24,5 @@ func Route(db *sql.DB) {
 	http.HandleFunc("/dislikecomment", handler.DislikeComment(db))
 	http.HandleFunc("/search",handler.Search(db))
 	http.HandleFunc("/filter",handler.Filter(db))
+	http.HandleFunc("/filtermypage",handler.FilterMyPage(db))
 }
